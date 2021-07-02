b17b4c28c601645ef5314c84ae3e6bd071b7d8ef
aleksandar <aca.djordjevic1113@gmail.com>
2021-07-02 12:51:09 -0700

    createUser fucntion added

--- functions.js
+++ functions.js
@@ -3,7 +3,15 @@ const functions = {
     
     isNull: () => null,
 
-    checkValue: (x) => x
+    checkValue: (x) => x,
+
+    createUser: () => {
+        const user = {firstName: 'Brad'}
+
+        user['lastName'] = 'Traversy';
+
+        return user;
+    }
 
 };
 
--- functions.test.js
+++ functions.test.js
@@ -16,11 +16,28 @@ test('Adds 2 + 2 to NOT EQUAL 5', () => {
     expect(functions.add(2, 2)).not.toBe(5);
 }); 
 
-
+//toBeNull
 test('Should be null', () => {
     expect(functions.isNull()).toBeNull();
 }); 
 
+//toBeFlasy
 test('Should be falsy', () => {
     expect(functions.checkValue(null)).toBeFalsy();
 }); 
+
+//toEqual - for objects and array
+test('User should be Brad Trabersy object', () => {
+    expect(functions.createUser()).toEqual({
+        firstName: 'Brad',
+        lastName: 'Traversy'
+    });
+});
+
+
+//less than and greater than
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThen(1600);
+})
