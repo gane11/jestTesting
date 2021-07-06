680565bdf21797a8d41138da1df32a2577654a2b
aleksandar <aca.djordjevic1113@gmail.com>
2021-07-05 22:08:11 -0700

    testing promises

--- functions.test.js
+++ functions.test.js
@@ -65,10 +65,10 @@ test("User fetched name should be Leanne Graham", async () => {
 });
 
 //promise
-// test('User fetched name should be Leanne Graham', () => {
-//     expect.assertions(1);
-//     return functions.fetchUser()
-//     .then(data => {
-//         expect(data.name).toEqual('Leanne Graham')
-//     })
-// })
+test('User fetched name should be Leanne Graham', () => {
+    expect.assertions(1);
+    return functions.fetchUser()
+    .then(data => {
+        expect(data.name).toEqual('Leanne Graham')
+    })
+})
