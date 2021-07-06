ff3f26c6d8a2cadf340f979993d4e9c4de1cbf8e
aleksandar <aca.djordjevic1113@gmail.com>
2021-07-06 15:28:28 -0700

    testinf isAnagram fucntion

---
+++ anagram.test.js
@@ -0,0 +1,19 @@
+const isAnagram = require('./anagram');
+
+test('isAnagram function exists' ,() => {
+    expect(typeof isAnagram).toEqual('function');
+});
+
+test('"cinema" is anagram of "iceman"', () => {
+    expect(isAnagram('cineam', 'iceman')).toBeTruthy();
+});
+
+
+test('"Dormitory" is anagram of "dirty room##"', () => {
+    expect(isAnagram('Dormitory', 'dirty room##')).toBeTruthy();
+});
+
+
+test('"Hello" is not anagram of "Aloha"', () => {
+    expect(isAnagram('Hello', 'Aloha')).toBeFalsy();
+});
