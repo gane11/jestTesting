b2708e4329b39452de97b20622f77836dcc65663
aleksandar <aca.djordjevic1113@gmail.com>
2021-07-05 22:07:52 -0700

    testing fetch calls

--- functions.test.js
+++ functions.test.js
@@ -38,6 +38,37 @@ test('User should be Brad Trabersy object', () => {
 //less than and greater than
 test('Should be under 1600', () => {
     const load1 = 800;
-    const load2 = 700;
-    expect(load1 + load2).toBeLessThen(1600);
+    const load2 = 670;
+    expect(load1 + load2).toBeLessThan(1600);
 })
+
+
+//regex
+test('There is no I in team', () => {
+    expect('team').not.toMatch(/I/i);
+})
+
+//arrays 
+test('Admin should be in usernames', () => {
+    userNames = ['alex' ,'karen', 'admin'];
+    expect(userNames).toContain('admin');
+})
+
+
+//working with async data
+
+//async
+test("User fetched name should be Leanne Graham", async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+    expect(data.name).toEqual("Leanne Graham");
+});
+
+//promise
+// test('User fetched name should be Leanne Graham', () => {
+//     expect.assertions(1);
+//     return functions.fetchUser()
+//     .then(data => {
+//         expect(data.name).toEqual('Leanne Graham')
+//     })
+// })
