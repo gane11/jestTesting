d77debfe5f31c74d4c441f6e10e375a9b4c58d63
aleksandar <aca.djordjevic1113@gmail.com>
2021-07-06 14:30:24 -0700

    testing the function

---
+++ reverseString.test.js
@@ -0,0 +1,10 @@
+const reverseString = require('./reverseString');
+
+
+test('reverseString function exists', () => {
+    expect(reverseString).toBeDefined();
+});
+
+test('String reverses', () => {
+    expect(reverseString('hello')).toEqual('olleh');
+});
